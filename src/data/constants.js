ac5c328141cd23e7d53a25dbf2ca97abbfad8026
Saurabh Dahibhate <saurabhdahibhate50@gmail.com>
2024-03-26 22:06:27 +0530

    final touch

---
+++ src/data/constants.js
@@ -0,0 +1,245 @@
+export const Bio = {
+  name: "Saurabh Dahibhate",
+  roles: ["Front-end Developer", "React Developer", "WordPress Developer"],
+  description:
+    "I am a dedicated front-end developer driven by a passion for crafting seamless user experiences. I thrive on creating innovative solutions that delight users and exceed expectations.",
+  github: "https://github.com/SaurabhDahibhate/",
+  resume:
+    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/saurabh-dahibhate/",
+  insta: "https://www.instagram.com/its_ds_18/",
+  facebook: "https://www.facebook.com/saurabh.dahibhate.7/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
+      },
+
+      {
+        name: "Php",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg",
+      },
+      {
+        name: "MySql",
+        image:
+          "https://upload.wikimedia.org/wikipedia/labs/8/8e/Mysql_logo.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "WordPress",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/98/WordPress_blue_logo.svg",
+      },
+      {
+        name: "Elementor",
+        image:
+          "https://images.ctfassets.net/sw4ojjqn6qvl/4h4hsWboffhHFaByIQx7zf/8bfc96257e2338c3c6d3fe6a80770ce4/elementor-logo.svg",
+      },
+      {
+        name: "Git",
+        image: "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg",
+      },
+      {
+        name: "Jenkins",
+        image:
+          "https://sg.mirror.servanamanaged.com/jenkins/art/jenkins-logo/96x96/logo.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://res.cloudinary.com/dsgdhuhbp/image/upload/v1709914893/omi-infotech_wsxtu5.jpg",
+    role: "Web Developer",
+    company: "Omi Infotech Pvt Ltd",
+    date: "Jan 2022 - Present",
+    desc: "Assisting the development team in maintaining 8 websites • Optimized page elements to reduce loading time and debugged code to ensure cross-browser usability. • Developing and implementing user interface components using JavaScript, HTML, CSS, and frameworks like Reac",
+    skills: [
+      "HTML5",
+      "CSS3",
+      "BootStrap",
+      "Tailwind",
+      "JavaScript",
+      "WordPress",
+      "Php",
+      "ReactJS",
+    ],
+    // doc: "https://res.cloudinary.com/dsgdhuhbp/image/upload/v1709966596/vibeosys_exp_tjwxar.jpg",
+  },
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/dsgdhuhbp/image/upload/v1709965174/Vibeosys_ngzdw2.jpg",
+    role: "Digital Marketing Intern",
+    company: "VibeosysSoftware Pvt Ltd",
+    date: "Aug 2021 - Nov 2021",
+    desc: "• Regularly optimized the company’s website for improved performance and user experience. • Proactively identified and resolved website issues related to optimization in WordPress. • Implemented SEO strategies to improve the company’s visibility on Google Search",
+    skills: ["HTML", "CSS", "Php", "MySql", "WordPress"],
+    doc: "https://res.cloudinary.com/dsgdhuhbp/image/upload/v1709966596/vibeosys_exp_tjwxar.jpg",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://res.cloudinary.com/dsgdhuhbp/image/upload/v1709979631/download_r1ccjd.png",
+    school: "ISB&M College of Engineering, Pune",
+    date: "June 2019 - July 2022",
+    grade: "9.27 CGPA",
+    desc: "As a Computer Engineering student, I am enjoyed a journey of continuous learning and exploration. I am learned  the Web development, data structures, and computer systems, while also developed proficiency in programming languages and software development. This field offers the opportunity to innovate and create technologies that can transform the world. I am well knowned key to success is curiosity, persistence, and a passion for problem-solving.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/dsgdhuhbp/image/upload/v1709979896/1600936394main_1_g5b5ym.jpg",
+    school: "Ekalavya Shikshan Sanstha's Polytechnic, Pune ",
+    date: "May 2016 - June 2019",
+    grade: "66.12%",
+    desc: "As a Computer Diploma college student, I am stepped into an exciting realm of technology and innovation. My education has provided me with a fundamental understanding of computer systems, programming languages, and software applications. I have learnt to design, develop, and troubleshoot computer software and hardware, equipping me with skills that are highly valued in today’s digital world. The journey of learnt in this field is filled with challenges and rewards, and your my and dedication will be my greatest assets.",
+    degree: "Diploma in Computer Technology",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "BlogWrite",
+    date: "Feb 2024",
+    description:
+      "Designed and engineered a cutting-edge blogging platform using ReactJS and Tailwind CSS, boasting a visually stunning, dark-mode compatible interface. Utilized the power of Appwrite as the backend infrastructure, enabling seamless user authentication and robust CRUD operations for managing blog content. Leveraged Redux Toolkit for efficient state management, while React Routing facilitated smooth navigation throughout the platform. Implemented intuitive file handling features, empowering users to personalize their posts with ease by selecting and uploading images, enhancing the overall user experience and interactivity.",
+    image:
+      "https://res.cloudinary.com/dsgdhuhbp/image/upload/v1711451319/blogwrite-app-_vmqedq.gif",
+    tags: [
+      "ReactJS",
+      "Tailwind CSS",
+      "Appwrite",
+      "Redux-Toolkit",
+      "React-Routing",
+      "React-Form",
+    ],
+    github:
+      "https://github.com/SaurabhDahibhate/BlogWrite-React-Appwrite-Project",
+    webapp: "https://blog-write.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "HealthyFoodies",
+    date: "Nov 2023",
+    description:
+      "It features smooth scrolling animations and utilizes the Intersection Observer API for sticky navigation. The project implements a flexbox layout for responsive page design and integrates external libraries like Ionicons and Smoothscroll polyfill. With a focus on semantic HTML and form handling for user sign-up, OmniFood showcases expertise in modern web development technique",
+    image:
+      "https://res.cloudinary.com/dsgdhuhbp/image/upload/v1709992201/healthfoodie_lzgufu.gif",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Flexbox",
+      "Intersection Observer API",
+      "Ionicons",
+      "Smoothscroll polyfil",
+    ],
+    github: "https://github.com/SaurabhDahibhate/foodwebsite",
+    webapp: "https://healthyf00dies.netlify.app/",
+  },
+
+  {
+    id: 1,
+    title: "Programmer Laughter",
+    date: "Jan 2024",
+    description:
+      "This project showcases dynamic content fetching and display using JavaScript's Fetch API to retrieve programming jokes from an external API. The fetched joke is then displayed along with a randomly selected GIF for added humor. The webpage features a responsive design and incorporates event listeners for user interaction, such as fetching new jokes and copying jokes to the clipboard. Utilizing modern web development techniques, the project demonstrates proficiency in asynchronous programming, DOM manipulation, and user experience enhancement. With its clean code structure and seamless integration of external resources, this project exemplifies expertise in frontend development and creative implementation of APIs for engaging user experiences",
+    image:
+      "https://res.cloudinary.com/dsgdhuhbp/image/upload/v1709993407/joke_iiigwz.gif",
+    tags: ["Javascript", "Fetch Api", "DOM manipulation"],
+    github: "https://github.com/SaurabhDahibhate/Programmer_joke_generator",
+    webapp: "https://programmer-jokie.netlify.app/",
+  },
+  // {
+  //   id: 3,
+  //   title: "Red Soil Boating Club Hadshi",
+  //   date: "Nov 2022",
+  //   description:
+  //     "I have designed, developed and deployed this Boating Website using WordPress ",
+  //   image:
+  //     "https://res.cloudinary.com/dsgdhuhbp/image/upload/v1709994184/boating_cv5wwp.gif",
+  //   tags: ["Designing", "WordPress Development", "SEO", "Hosting"],
+  //   github: "",
+  //   webapp:
+  //     "https://drive.google.com/drive/folders/1Z0VFD4qa_5f4Ru22f3tlRN7Jbd33Qr9o?usp=drive_link",
+  // },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
